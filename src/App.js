cca1208112af94ff1d60f485d5707f40296676e2
DEAMBROGGI <adrian.m.deambroggi@gmail.com>
2022-01-04 18:54:27 -0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import React from 'react'
 import Lamp from './components/Lamp'
 import Bulb from './components/Bulb'
 import Texto from './components/Texto'
 
 const App = () =>{
-  let valor = 300
+  const [valor,setValor] = React.useState(300)
+
+  const addValor=()=>{
+
+    setValor(valor+1)
+
+  }
+
+  
   let texto ="texto"
   return (
     <>
     <Texto valor={valor} texto={texto}/>
     <Lamp />
-    
+    <button onClick={addValor}>AGREGAR VALOR</button>
+    <button onClick={()=>setValor(valor-1)}>RESTAR VALOR</button>
     </>
   );
 }
