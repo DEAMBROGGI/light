56dc3518710e5d3ceeac730344eb9cfbef64dff5
DEAMBROGGI <adrian.m.deambroggi@gmail.com>
2022-01-03 21:26:40 -0300

    init

--- src/components/Bulb.js
+++ src/components/Bulb.js
@@ -4,7 +4,7 @@ export default function Bulb({ lampara,test,props }) {
   
   return (
   <div>
-    <h1>{valor}</h1>
+    <h1>{props.valor}</h1>
     <h1>{test}</h1>
       {lampara ?
         (<span>
