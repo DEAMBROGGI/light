3463441a632d01fff03450783d11e34571d70450
DEAMBROGGI <adrian.m.deambroggi@gmail.com>
2022-01-04 19:18:38 -0300

    change

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import Texto from './components/Texto'
 
 const App = () =>{
   const [valor,setValor] = React.useState(300)
-
+  const [texto, setTexto] = React.useState('')
+  
   const addValor=()=>{
 
     setValor(valor+1)
 
   }
 
+  console.log(texto)
   
-  let texto ="texto"
   return (
     <>
     <Texto valor={valor} texto={texto}/>
     <Lamp />
     <button onClick={addValor}>AGREGAR VALOR</button>
     <button onClick={()=>setValor(valor-1)}>RESTAR VALOR</button>
+    <input onChange={e=>setTexto(e.target.value)} ></input>
     </>
   );
 }
--- src/components/Texto.js
+++ src/components/Texto.js
@@ -2,11 +2,15 @@
 
 export default function Text({valor, texto}){
 
+    console.log()
 return(
+    <>
     <div className="nav">
-    <h1>{texto}</h1>
+    
     <h1>{valor}</h1>
     </div>
+    <p>{texto}</p>
+    </>
 )
 
 
