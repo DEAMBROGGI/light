22c3cf46b22ec29cf3684f507a92ff2ae0a19bdd
DEAMBROGGI <adrian.m.deambroggi@gmail.com>
2022-06-14 13:07:51 -0300

    controllers, model, routs

---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+const Router = require('express').Router();
+
+const citiesControllers = require('../controllers/citiesControllers');
+const {getCities, getOneCity, addCity, modifyCity, removeCity,addMultipleCity} = citiesControllers
+
+Router.route('/cities')
+.get(getCities)
+.post(addCity)
+
+Router.route('/cities/:id') 
+.delete(removeCity)
+.put(modifyCity) 
+.get(getOneCity)
+
+Router.route("/addMultipleCity")
+.post(addMultipleCity)
+module.exports = Router
